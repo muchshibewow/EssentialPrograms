75df67cdbe60c0e4535f731e039cd18979cd3e2f
Pravin Pratap <pratappravin250@gmail.com>
2017-09-19 01:54:05 +0530

    Add files via upload

---
+++ SieveOfEratosthenes_CPP.cpp
@@ -0,0 +1,45 @@
+// Sieve of Eratosthenes implementation in C++.
+
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	long n;
+	cout<<"Enter number : ";
+	cin>>n;
+	bool primes[n+1];
+
+	// Initialising the primes array, assuming every number is a prime.
+
+	for(auto x:primes)
+	{
+		x=true;
+	}
+
+	// Changing the multiples of prime factors to be non-prime.
+	// Also, 0 and 1 are not considered prime numbers.
+
+	primes[0]=false;
+	primes[1]=false;
+
+	for(auto i=2;i*i<=n;++i)
+	{
+		for(auto j=i*i;j<=n;j+=i)
+		{
+			primes[j]=false;
+		}
+	}
+
+	for(auto i=2;i<=n;++i)
+	{
+		if(primes[i])
+		{
+			cout<<i<<" ";
+		}
+	}
+	cout<<"\n";
+	return 0;
+}
